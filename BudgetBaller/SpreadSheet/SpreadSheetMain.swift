a5173baa36bb4f0ad80a8e6aa700c2c814b84b96
angkumar <angkumar787@gmail.com>
2025-05-09 23:01:27 -0700

    new stuff

--- BudgetBaller/SpreadSheet/SpreadSheetMain.swift
+++ BudgetBaller/SpreadSheet/SpreadSheetMain.swift
@@ -47,7 +47,7 @@ struct SpreadSheetMain: View {
                 Text("Budget Baller")
                     .font(.system(size: 60, weight: .bold, design: .default))
                     .foregroundColor(.white)
-                Text("Your Spreadsheet Is Here!")
+                Text("Your Budget Is Here!")
                     .font(.system(size: 30, weight: .light, design: .default))
                     .foregroundColor(.white)
             }
