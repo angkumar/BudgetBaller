4f58135dfddc0b17a4233f8f9a9675a420c17b3c
angkumar <angkumar787@gmail.com>
2025-06-09 16:03:15 -0400

    new stuff

--- BudgetBaller/main/Personalize/Settings.swift
+++ BudgetBaller/main/Personalize/Settings.swift
@@ -43,7 +43,7 @@ struct Settings: View {
                     
                     List {
                         
-                        Text("HEE HEE Sigma")
+                        Text("HEE HEE")
                             .font(.largeTitle)
                         
                         DisclosureGroup("Home", isExpanded: $homeExpanded) {
