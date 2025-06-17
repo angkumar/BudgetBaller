26775bf286ff76477fe9390902d7476e16e3403e
angkumar <angkumar787@gmail.com>
2025-06-16 21:08:11 -0400

    new

--- BudgetBaller/main/Personalize/Settings.swift
+++ BudgetBaller/main/Personalize/Settings.swift
@@ -43,7 +43,7 @@ struct Settings: View {
                     
                     List {
                         
-                        Text("HEE BRO")
+                        Text("bro")
                             .font(.largeTitle)
                         
                         DisclosureGroup("bro", isExpanded: $homeExpanded) {
